d548926b8947557e27e4f7690181f2f0cebc65c7
Alexandru <alexander78xzx@gmail.com>
2025-12-17 23:30:14 +0100

    Added jumps

--- main.py
+++ main.py
@@ -32,6 +32,11 @@ white = pygame.transform.scale(white, (100,100))
 red = pygame.image.load("red.png").convert_alpha()
 red = pygame.transform.scale(red, (100,100))
 
+pos1 = None
+pos2 = None
+selected_piece = None
+target = None
+
 def draw_board():
     imp = pygame.image.load("board.png").convert()
     imp = pygame.transform.scale(imp, (800,800))        #load the board image
@@ -44,17 +49,112 @@ def draw_figs():
                 screen.blit(black, (100*i, 100*j))
             elif board[j][i]==1:
                 screen.blit(white, (100*i, 100*j))
+
+
 def draw_available_moves(pos1,pos2):
     if board[pos1][pos2]==1:
-        if board[pos1-1][pos2+1]==0 or board[pos1-1][pos2-1]==0:
-            screen.blit(red, (100*(pos2+1), 100*(pos1-1)))
-            screen.blit(red, (100*(pos2-1), 100*(pos1-1)))
-        else:
-            pass
+        if pos2 + 1 <= 7:
+            if board[pos1-1][pos2+1]==0:
+                screen.blit(red, (100*(pos2+1), 100*(pos1-1)))
+        if pos2-1>=0:
+            if board[pos1-1][pos2-1]==0:
+                screen.blit(red, (100*(pos2-1), 100*(pos1-1)))
+
+    if board[pos1][pos2]==2:
+        if pos2 + 1 <= 7:
+            if board[pos1+1][pos2+1]==0:
+                screen.blit(red, (100*(pos2+1), 100*(pos1+1)))
+        if pos2-1>=0:
+            if board[pos1+1][pos2-1]==0:
+                screen.blit(red, (100*(pos2-1), 100*(pos1+1)))
+
+def move(selected_piece,target):
+    if board[selected_piece[0]][selected_piece[1]]==1:
+        # jump logic
+        if selected_piece[0] >= 2 and selected_piece[1] >= 2:
+            if target==(selected_piece[0]-2,selected_piece[1]-2): # top left
+                if board[selected_piece[0]-1][selected_piece[1]-1]==2:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 1
+                        board[selected_piece[0]-1][selected_piece[1]-1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] >= 2 and selected_piece[1] <= 5:
+            if target==(selected_piece[0]-2,selected_piece[1]+2): # top right
+                if board[selected_piece[0]-1][selected_piece[1]+1]==2:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 1
+                        board[selected_piece[0]-1][selected_piece[1]+1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] <= 5 and selected_piece[1] >= 2:
+            if target==(selected_piece[0]+2,selected_piece[1]-2): # bottom left
+                if board[selected_piece[0]+1][selected_piece[1]-1]==2:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 1
+                        board[selected_piece[0]+1][selected_piece[1]-1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] <= 5 and selected_piece[1] <= 5:
+            if target==(selected_piece[0]+2,selected_piece[1]+2): # bottom right
+                if board[selected_piece[0]+1][selected_piece[1]+1]==2:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 1
+                        board[selected_piece[0]+1][selected_piece[1]+1] = 0
+                        selected_piece = None
+                        return
+        if (target==(selected_piece[0]-1,selected_piece[1]-1) or target==(selected_piece[0]-1,selected_piece[1]+1)) and board[target[0]][target[1]]==0 :
+            board[selected_piece[0]][selected_piece[1]] = 0
+            board[target[0]][target[1]] = 1
+            selected_piece = None
+    elif board[selected_piece[0]][selected_piece[1]]==2:
+        # jump logic
+        if selected_piece[0] >= 2 and selected_piece[1] >= 2:
+            if target==(selected_piece[0]-2,selected_piece[1]-2): # top left
+                if board[selected_piece[0]-1][selected_piece[1]-1]==1:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 2
+                        board[selected_piece[0]-1][selected_piece[1]-1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] >= 2 and selected_piece[1] <= 5:
+            if target==(selected_piece[0]-2,selected_piece[1]+2): # top right
+                if board[selected_piece[0]-1][selected_piece[1]+1]==1:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 2
+                        board[selected_piece[0]-1][selected_piece[1]+1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] <= 5 and selected_piece[1] >= 2:
+            if target==(selected_piece[0]+2,selected_piece[1]-2): # bottom left
+                if board[selected_piece[0]+1][selected_piece[1]-1]==1:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 2
+                        board[selected_piece[0]+1][selected_piece[1]-1] = 0
+                        selected_piece = None
+                        return
+        if selected_piece[0] <= 5 and selected_piece[1] <= 5:
+            if target==(selected_piece[0]+2,selected_piece[1]+2): # bottom right
+                if board[selected_piece[0]+1][selected_piece[1]+1]==1:
+                    if board[target[0]][target[1]]==0:
+                        board[selected_piece[0]][selected_piece[1]] = 0
+                        board[target[0]][target[1]] = 2
+                        board[selected_piece[0]+1][selected_piece[1]+1] = 0
+                        selected_piece = None
+                        return
+        if (target==(selected_piece[0]+1,selected_piece[1]-1) or target==(selected_piece[0]+1,selected_piece[1]+1)) and board[target[0]][target[1]]==0 :
+            board[selected_piece[0]][selected_piece[1]] = 0
+            board[target[0]][target[1]] = 2
+            selected_piece = None
 
 
-pos1 = None
-pos2 = None
 running = True
 while running:
     for event in pygame.event.get():        #game loop
@@ -62,16 +162,25 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             position = event.pos
-            #selected = board[position[0]//100][position[1]//100]
             pos1 = position[1]//100
             pos2 = position[0]//100
             selected = board[position[1]//100][position[0]//100]
+            if selected==1 or selected==2 and selected_piece==None:
+                selected_piece = (pos1,pos2)
+            elif selected_piece!=None and selected==0:
+                target = (pos1,pos2)
+                move(selected_piece,target)
+                selected_piece = None
+                pos1 = None
+                pos2 = None
+
             print(pos1,pos2)
-            #board[position[1]//100][position[0]//100] = 0   
     draw_board()
     draw_figs()
     if pos1!=None and pos2!=None:
         draw_available_moves(pos1,pos2)
+
+
     pygame.display.flip()
 
 pygame.quit()
